44af8b311af77e71ce5181e1ac1fcc91900ca19a
Matthew <Deweasematthew@gmail.com>
2023-08-15 00:56:16 -0500

    adding JavaScript Clock

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.body.addEventListener('mousemove', function(event) {
+        let x = event.clientX;
+        let y = event.clientY;
+        let screenWidth = window.innerWidth;
+        let screenHeight = window.innerHeight;
+        let red = Math.round((x / screenWidth) * 255);
+        let blue = Math.round((y / screenHeight) * 255);
+        document.body.style.background = 'linear-gradient(45deg, rgb(${red}, 100, ${blue}), rgb(100, ${blue}, ${red}))';
+    
+    })
+})
+
 function toggleDarkMode() {
     const element = document.documentElement;
     element.classList.toggle("dark-mode");
